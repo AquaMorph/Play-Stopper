bcec04638bdfd265d077389532ad6566cb438e11
Christian Colglazier <aquamorphcomputing@gmail.com>
2015-03-17 19:18:59 -0400

    Added actionbar

--- mobile/src/main/java/com/aquamorph/playstopper/MainActivity.java
+++ mobile/src/main/java/com/aquamorph/playstopper/MainActivity.java
@@ -9,10 +9,14 @@ import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity implements OnClickListener, OnSharedPreferenceChangeListener {
+public class MainActivity extends ActionBarActivity implements OnClickListener, OnSharedPreferenceChangeListener {
 
+	private final String TAG = "MainActivity";
     private final long interval = 1000;
     private long numberSeconds = 0;
     private long numberMinutes = 0;
@@ -21,7 +25,18 @@ public class MainActivity extends Activity implements OnClickListener, OnSharedP
     static boolean userChoice = true;
     Notifications notifications = new Notifications();
     Timer clock = new Timer();
-    Toolbar toolbar;
+
+	//Menu Options
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()){
+			case R.id.action_settings:
+				Log.i(TAG, "Settings Item Clicked");
+				openSettings();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -116,6 +131,13 @@ public class MainActivity extends Activity implements OnClickListener, OnSharedP
             }
         });*/
     }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
     @Override
     protected void onPause() {
