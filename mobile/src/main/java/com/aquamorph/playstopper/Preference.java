1b2973f5a6641f63bb8ab4465594116e7d11173d
Christian Colglazier <aquamorphcomputing@gmail.com>
2015-05-11 11:56:13 -0400

    Added actionbar to the preference menu

--- mobile/src/main/java/com/aquamorph/playstopper/Preference.java
+++ mobile/src/main/java/com/aquamorph/playstopper/Preference.java
@@ -4,10 +4,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Preference extends PreferenceActivity {
 
-    @SuppressWarnings("deprecation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         MainActivity.theme(this);
@@ -18,5 +22,19 @@ public class Preference extends PreferenceActivity {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         boolean autoStart = prefs.getBoolean("checkBox1", true);
         System.out.println(autoStart);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
+        root.addView(bar, 0); // insert at top
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
